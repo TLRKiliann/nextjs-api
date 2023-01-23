b272e2c938c82043ffd55272cf3f6cc536b8e16d
TLRKiliann <philogenie@protonmail.com>
2023-01-23 19:38:43 +0100

    form replaced by useEffect

--- my-api/pages/comments/index.tsx
+++ my-api/pages/comments/index.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const CommentsPage = () => {
 
   const [comments, setComments] = useState<Array<string>>([])
   const [comment, setComment] = useState<string>("")
-  
+
+  useEffect(() => {
+    const MyFunction = async () => {
+      const response = await fetch("/api/comments")
+      const data = await response.json()
+      setComments(data)
+    }
+    MyFunction()
+    return console.log("useEffect updated !")
+  }, [comment])
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setComment(event.target.value)
   }
 
-  const handleSubmit = async (event: React.ChangeEvent<HTMLFormElement>) => {
-    event.prevent.default()
+  const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement>, comment: string) => {
+    console.log(comment, "comment")
     const response = await fetch("/api/comments", {
       method: "POST",
       body: JSON.stringify({comment}),
@@ -19,6 +29,7 @@ const CommentsPage = () => {
       }
     })
     const data = await response.json()
+    setComment("")
   }
 
   const handleComments = async () => {
@@ -28,14 +39,12 @@ const CommentsPage = () => {
   }
   return (
     <div>
-      <form onSubmit={(event) => handleSubmit(event, comment)}>
-        <input type='text' value={comment} onChange={(event) => handleChange(event)} />
-        <button type='submit'>Enter</button>
-      </form>
+      <input type='text' value={comment} onChange={(event) => handleChange(event)} />
+      <button type='button' onClick={(event) => handleSubmit(event, comment)}>Enter</button>
       <button type='button' onClick={handleComments}>Load Comments</button>
       {comments.map((comment: any) => (
         <p key={comment.id}>{comment.id} {comment.text}</p>
-        ))}
+      ))}
     </div>
   )
 }

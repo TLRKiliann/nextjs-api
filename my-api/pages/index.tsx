dc654bb0905a7885b658c921f3a49413a14f4145
TLRKiliann <philogenie@protonmail.com>
2023-05-03 09:59:11 +0200

    type improved

--- my-api/pages/index.tsx
+++ my-api/pages/index.tsx
@@ -4,27 +4,34 @@ import Image from 'next/image'
 import { z } from 'zod'
 
 type SpaceXData = {
-  name: z.string;
-  links: [
-    patch: [
-      large: z.string,
+  data: {
+    name: z.string
+    date_local: z.string
+    links: [
+      patch: [
+        large: z.string
+      ]
     ]
-  ]
+  }
+  error: {
+    message: z.string
+  }
+  isLoading: z.boolean;
+  isError: z.boolean;
 }
 
-//const getSpaceXData: z.string = "https://api.spacexdata.com/v5/launches/latest"
+const getSpaceXData: z.string = "https://api.spacexdata.com/v5/launches/latest"
 
-const getSpaceXData = () => {
-  fetch("https://api.spacexdata.com/v5/launches/latest").then((res) => res.json())
-}
-
+//queryFn = function fetch a constant !
 const Home = () => {
   const { data, isLoading, isError, error } = useQuery<SpaceXData>({
     queryKey: ['spacex'],
     queryFn: () => fetch(getSpaceXData).then((res) => res.json())
   });
   
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
   
   if (!data) {
     return <div>No data</div>
@@ -47,7 +54,7 @@ const Home = () => {
         width={400}
         height={375}
         alt="patch-img"
-        style={{width: "60%", height:"auto"}}
+        style={{width: "20%", height:"auto"}}
       />
     </div>
   )
